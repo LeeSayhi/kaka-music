325dcd2ad5c8454df27aa03dd9f9d7a34a8ea85e
leesay <leesayhi@126.com>
2017-11-15 18:41:04 +0800

    player animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,30 +1,157 @@
 <template>
   <div class="player" v-show="playList.length > 0">
-    <div class="normal-player" v-show="fullScreen">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img src="" width="100%" height="100%">
+        </div>
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="icon-back"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapepr" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+            <div class="playing-lyric-wrapper">
+              <div class="playing-lyric"></div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="dot-wrapper">
+            <span class="dot active"></span>
+            <span class="dot"></span>
+          </div>
+          <div class="progress-wrapper">
+            <span class="time time-l"></span>
+            <div class="progress-bar-wrapper">
 
-    </div>
-    <div class="mini-player" v-show="!fullScreen">
-      
-    </div>
+            </div>
+            <span class="time time-r"></span>
+          </div>
+          <div class="operators"></div>
+        </div>
+      </div>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img :src="currentSong.image" width="40" height="40">
+        </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control"></div>
+        <div class="control"></div>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
-  import {mapGetters} from 'vuex'
+  import {mapGetters, mapMutations} from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import {prefixStyle} from 'common/js/dom'
+
+  const transform = prefixStyle('transform')
 
   export default {
     computed: {
       ...mapGetters([
         'playList',
         'fullScreen',
-        'sequenceList',
-        'currentIndex',
-        'playing'
+        'currentSong'
       ])
+    },
+    methods: {
+      ...mapMutations({
+        setFullScreen: 'SET_FULL_SCREEN'
+      }),
+      back () {
+        this.setFullScreen(false)
+      },
+      open () {
+        this.setFullScreen(true)
+      },
+      // 计算出 normal -> mini 的偏移量以及比例
+      _getPosAndScale () {
+        const targetWidth = 40 // mini
+        const paddingLeft = 40 // mini
+        const paddingBottom = 30  // mini
+        const paddingTop = 80  // normal
+        const width = window.innerWidth * 0.8   // normal
+
+        const scale = targetWidth / width
+        const x = -(window.innerWidth / 2 - paddingLeft)  // x 偏移
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom  // y 偏移
+
+        return {
+          x,
+          y,
+          scale
+        }
+      },
+      /* 进入 */
+      enter (el, done) {
+        const {x, y, scale} = this._getPosAndScale()
+
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0, 0, 0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0, 0, 0) scale(1.1)`
+          }
+        }
+
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 600,
+            easing: 'linear'
+          }
+        })
+
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter () {
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      /* 离开 */
+      leave (el, done) {
+        const {x, y, scale} = this._getPosAndScale()
+
+        this.$refs.cdWrapper.style.transition = 'all 0.6s'
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave () {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
+      }
     }
   }
 </script>
 <style lang="stylus">
   @import '~common/stylus/variable.styl'
+  @import '~common/stylus/mixin.styl'
 
   .player
     .normal-player
@@ -33,6 +160,136 @@
       right: 0
       bottom: 0
       left: 0
-      overflow: hidden
+      z-index: 150
       background: $color-background
+      .background
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 28px
+        .back
+          position: absolute
+          top: 0
+          left: 6px
+          z-index: 50
+          .icon-back
+            display: block
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          width: 70%
+          margin: 0 auto
+          line-height: 40px
+          text-align: center
+          no-wrap()
+          font-size: $font-size-large
+          color: $color-text
+        .subtitle
+          line-height: 20px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-text        
+      .middle
+        position: fixed
+        width: 100%
+        top: 80px
+        bottom: 170px
+        white-space: nowrap
+        .middle-l
+          position: relative
+          width: 100%
+          height: 0
+          padding-top: 80%
+          .cd-wrapepr
+            position: absolute
+            left: 10%
+            top: 0
+            width: 80%
+            height: 100%
+            .cd
+              width: 100%
+              height: 100%
+              box-sizing: border-box
+              border: 10px solid rgba(255, 255, 255, 0.1)
+              border-radius: 50%
+              .image
+                position: absolute
+                left: 0
+                top: 0
+                width: 100%
+                height: 100%
+                border-radius: 50%
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .dot-wrapper
+          text-align: center
+          font-size: 0
+          .dot
+            display: inline-block
+            vertical-align: middle
+            margin: 0 4px
+            width: 8px
+            height: 8px
+            border-radius: 50%
+            background: $color-text-l
+            &.active
+              width: 20px
+              border-radius: 5px
+              background: $color-text-ll
+      &.normal-enter-active, &.normal-leave-active
+        transition: all 0.6s
+        .top, .bottom
+          transition: all 0.6s cubic-bezier(0.86, 0.18, 0.82, 1.32)         
+      &.normal-enter, &.normal-leave-to
+        opacity: 0
+        .top
+          transform: translate3d(0, -100px, 0)
+        .bottom
+          transform: translate3d(0, 100px, 0)
+    .mini-player
+      display: flex
+      align-items: center
+      position: fixed
+      left: 0
+      bottom: 0
+      z-index: 180
+      width: 100%
+      height: 60px
+      background: $color-highlight-background
+      &.mini-enter-active, &.mini-leave-active
+        transition: all 0.6s
+      &.mini-enter, &.mini-leave-to
+        opacity: 0
+      .icon
+        flex: 0 0 40px
+        width: 40px
+        padding: 0 10px 0 20px
+        img
+          border-radius: 50%
+      .text
+        display: flex
+        flex-direction: column
+        justify-content: center
+        flex: 1
+        line-height: 20px
+        .name
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d          
 </style>
--- src/store/state.js
+++ src/store/state.js
@@ -4,7 +4,7 @@ const state = {
 	singer: {}, // 歌手信息
   playing: false, // 播放 暂停
   fullScreen: false,  // 全屏播放
-  playList: [1, 2],  // 播放列表
+  playList: [],  // 播放列表
   sequenceList: [],  // 顺序播放列表
   mode: playMode.sequence, // 播放模式
   currentIndex: -1  // 当前播放索引
